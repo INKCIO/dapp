4a1157e6585752271818d0883b83259a89fb623b
inkcdev <kernone@qq.com>
2018-01-31 15:23:31 +0800

    parse cmd args

--- src/bin/bin.ts
+++ src/bin/bin.ts
@@ -2,8 +2,7 @@
 
 import * as commander from 'commander';
 
-commander.version('0.0.1')
-    .option('--rpc <addr>', 'ETH RPC Address', String)
+let args: { rpc: string, port: number } = <any>commander.version('1.0')
+    .option('--rpc <addr>', 'ETH RPC address', 'http://localhost:8545')
+    .option('--port [number]', 'Listening port', 5002)
     .parse(process.argv);
-
-    
