1633afbb5eba4f3184c5aa44d3238bfed0ff0aa7
inkcdev <kernone@qq.com>
2018-02-01 21:02:10 +0800

    add express typings defintions

--- src/icos/eos.ts
+++ src/icos/eos.ts
@@ -1,3 +1,10 @@
 import web3 from 'web3';
 import * as express from 'express';
 import axios from 'axios';
+
+const eos = '0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0';
+let geth: web3;
+
+export function initRPC(addr: string) {
+// geth = new web3(new ())
+}
