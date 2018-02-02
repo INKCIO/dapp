75da2f63876788f17a73bc086c91c767fa3fabcc
inkcdev <kernone@qq.com>
2018-02-02 21:28:05 +0800

    fork executable instance

--- src/bin/bin.ts
+++ src/bin/bin.ts
@@ -1,8 +1,24 @@
 #! /usr/bin/env node
 
 import * as commander from 'commander';
+import * as cluster from 'cluster';
+import * as os from 'os';
 
 let args: { rpc: string, port: number } = <any>commander.version('1.0')
     .option('--rpc <addr>', 'ETH RPC address', 'http://localhost:8545')
     .option('--port [number]', 'Listening port', 5002)
     .parse(process.argv);
+
+
+function fork() {
+    let worker = cluster.fork(process.env);
+    worker.once('exit', (code, signal) => {
+        fork();
+    });
+}
+
+if (cluster.isMaster) {
+    for (let i = 0; i < os.cpus().length; i++) {
+        fork();
+    }
+}
